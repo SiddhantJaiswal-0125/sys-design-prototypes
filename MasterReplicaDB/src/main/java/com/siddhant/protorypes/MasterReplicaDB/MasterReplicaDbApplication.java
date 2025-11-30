35d219e3bf6f0a3f93c3071d0b5b34b134b811cd
SiddhantJaiswal-0125 <siddhantjaiswal363@gmail.com>
2025-11-30 11:42:31 +0530

    initial commit

---
+++ MasterReplicaDB/src/main/java/com/siddhant/protorypes/MasterReplicaDB/MasterReplicaDbApplication.java
@@ -0,0 +1,13 @@
+package com.siddhant.protorypes.MasterReplicaDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterReplicaDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterReplicaDbApplication.class, args);
+	}
+
+}
---
+++ MasterReplicaDB/src/test/java/com/siddhant/protorypes/MasterReplicaDB/MasterReplicaDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siddhant.protorypes.MasterReplicaDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterReplicaDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
